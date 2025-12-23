3b2387d52066b834f6dd6e6b3821e9daa5d2f3b9
jennymakki <jjennyeklundd@hotmail.com>
2025-12-23 13:34:31 +0100

    fix: import on route

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
-import clientPromise from "../../../lib/mongodb";
+import { clientPromise } from "../../../lib/mongodb";
 
 export const authOptions = {
   adapter: MongoDBAdapter(clientPromise),
@@ -15,6 +15,3 @@ export const authOptions = {
     strategy: "jwt",
   },
 };
-
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
