f6d72cccd207399a078d3e432ccb22001bfe8d34
jennymakki <jjennyeklundd@hotmail.com>
2025-12-23 11:42:02 +0100

    fix: disable db and cloudinary code for initial deploy

--- src/app/api/recipes/route.js
+++ src/app/api/recipes/route.js
@@ -1,6 +1,3 @@
-import connectToDatabase from "../../lib/mongodb";
-import Recipe from "../../models/Recipe"; // skapa modell senare
-
 export async function GET() {
   return new Response(
     JSON.stringify({ message: "Recipes API not implemented yet" }),
