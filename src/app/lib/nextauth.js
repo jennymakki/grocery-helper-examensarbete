f7734e236c972c9d8c2e5e46881b639bd035d32c
jennymakki <jjennyeklundd@hotmail.com>
2025-12-23 13:13:21 +0100

    fix: use named import for clientPromise in NextAuth

--- src/app/lib/nextauth.js
+++ src/app/lib/nextauth.js
@@ -1,4 +1,4 @@
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
-import { clientPromise } from "./mongodb";
+import { clientPromise } from "../../../lib/mongodb";
 
 export default MongoDBAdapter(clientPromise);
