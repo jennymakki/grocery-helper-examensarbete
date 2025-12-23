992be90f8d33b85d7f050dd5ea6ea784bcf6df95
jennymakki <jjennyeklundd@hotmail.com>
2025-12-23 12:23:08 +0100

    testing path

--- src/app/api/recipes/route.js
+++ src/app/api/recipes/route.js
@@ -1,6 +1,16 @@
+import connectToDatabase from "../../../lib/mongodb";
+
 export async function GET() {
-  return new Response(
-    JSON.stringify({ message: "Recipes API not implemented yet" }),
-    { status: 200 }
-  );
+  try {
+    await connectToDatabase();
+    return new Response(
+      JSON.stringify({ status: "ok", message: "MongoDB connected!" }),
+      { status: 200 }
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ status: "error", message: error.message }),
+      { status: 500 }
+    );
+  }
 }
